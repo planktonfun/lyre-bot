617a41aac022152b3832ac55efe3ecb6d454ed02
EdmundYuen <swirling.dust@gmail.com>
2021-05-25 09:02:43 +0800

    Add command error handling with better messages

--- bot/exts/keymap_reader.py
+++ bot/exts/keymap_reader.py
@@ -1,6 +1,5 @@
 import logging
 import discord
-import io
 
 from discord.ext import commands
 
@@ -18,13 +17,28 @@ class KeymapReader(commands.Cog):
         self.lyre = Lyre.create()
         self.performer = Performer(self.lyre)
 
-    @commands.command()
-    async def read(self, ctx, notes: KeyMapParser):
+    @commands.command(aliases=("p", ""))
+    async def play(self, ctx, notes: KeyMapParser):
         async with ctx.channel.typing():
             await self.performer.load(notes)
             await self.performer.export(DEFAULT_WAV_PATH)
             with open(DEFAULT_WAV_PATH, "rb") as fp:
                 await ctx.send(file=discord.File(fp))
+
+    @play.error
+    async def play_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            if isinstance(error.original, discord.HTTPException):
+                await ctx.send("File upload has failed. The keymap might be too long.")
+            else:
+                await ctx.send(error)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(
+                "I did not detect any keymaps. Please surround your keymap with \`\`\` \`\``"
+            )
+        else:
+            await ctx.send(error)
+        raise error
 
 
 def setup(bot):
