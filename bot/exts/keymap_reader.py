e4e8b45c1b4dcefd55cea1e9a824e733e14e25f6
EdmundYuen <swirling.dust@gmail.com>
2021-06-04 15:47:15 +0800

    Send embed with raw repr of parsed notes

--- bot/exts/keymap_reader.py
+++ bot/exts/keymap_reader.py
@@ -1,14 +1,36 @@
 import logging
+import random
 
 import discord
+from discord import Embed
 from discord.ext import commands
 
+from bot.constants import DejaVu
 from bot.exts.converter import KeyMapParser
 from bot.exts.lyre import Lyre, Performer
 
 log = logging.getLogger(__name__)
 
 DEFAULT_WAV_PATH = "qiqi.wav"
+
+
+def create_embed(ctx: commands.Context):
+    author = ctx.author
+    notes = ctx.args[-1]
+    notes_repr = "".join(
+        f"{note.pitch:2} /{1000/note.duration:4n} | "
+        if i % 4 != 3
+        else f"{note.pitch:2} /{1000/note.duration:4n}\n"
+        for i, note in enumerate(notes)
+    )
+    if len(notes_repr) > 2042:
+        notes_repr = notes_repr[:2038] + "\n..."
+    description = f"```{notes_repr}```"
+    return Embed(
+        title="Lyre Bot Player",
+        description=description,
+        color=author.colour,
+    ).set_footer(text=random.choice(DejaVu.LYRICS))
 
 
 class KeymapReader(commands.Cog):
@@ -23,7 +45,7 @@ class KeymapReader(commands.Cog):
             await self.performer.load(notes)
             await self.performer.export(DEFAULT_WAV_PATH)
             with open(DEFAULT_WAV_PATH, "rb") as fp:
-                await ctx.send(file=discord.File(fp))
+                await ctx.send(embed=create_embed(ctx), file=discord.File(fp))
 
     @play.error
     async def play_error(self, ctx, error):
